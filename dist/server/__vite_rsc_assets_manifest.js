4d0cf69197186d43623441126653c0e5b67103f1
Federico Pellicciotta <federico.pellicciotta@acgroup.ai>
2026-09-09 14:39:40 +0200

    build: refresh 0.5.0 distributable frontend

--- dist/server/__vite_rsc_assets_manifest.js
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -1,86 +1,86 @@
 export default {
-  "clientEntryUrl": "/_next/static/chunks/index-CXF1N5Ls.js",
+  "clientEntryUrl": "/_next/static/chunks/index-TUS3iwfE.js",
   "clientEntryDeps": {
     "js": [
-      "/_next/static/chunks/index-CXF1N5Ls.js",
+      "/_next/static/chunks/index-TUS3iwfE.js",
       "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
       "/_next/static/chunks/framework-DTZGTDtF.js",
-      "/_next/static/chunks/vinext-C-LZuZrO.js"
+      "/_next/static/chunks/vinext-C8wwvras.js"
     ],
     "css": []
   },
   "clientReferenceDeps": {
     "6efdf509a785": {
       "js": [
-        "/_next/static/chunks/page-CII9yUB6.js",
+        "/_next/static/chunks/page-CIjy0qbX.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js",
-        "/_next/static/chunks/vinext-C-LZuZrO.js"
+        "/_next/static/chunks/vinext-C8wwvras.js",
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "9276801271d6": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "0b874ad30386": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "593f344dc510": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "bad85346fc72": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "15c18cfaeeff": {
       "js": [
-        "/_next/static/chunks/layout-segment-context-BrZARz9y.js",
+        "/_next/static/chunks/layout-segment-context-Dz1YVDLm.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/vinext-C8wwvras.js",
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "8c0f216c4604": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     },
     "89c3cac48cb5": {
       "js": [
-        "/_next/static/chunks/vinext-C-LZuZrO.js",
+        "/_next/static/chunks/vinext-C8wwvras.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-CXF1N5Ls.js"
+        "/_next/static/chunks/index-TUS3iwfE.js"
       ],
       "css": []
     }
@@ -89,7 +89,7 @@ export default {
     "app/layout.tsx": {
       "js": [],
       "css": [
-        "/_next/static/css/index.5BWdpYHX.css"
+        "/_next/static/css/index.BkimLMcz.css"
       ]
     }
   }
